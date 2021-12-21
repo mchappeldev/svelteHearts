feb9765cf09901c706a23b6384610d6fa892585d
Michael Chappel <michael.chappel@caddmicrosystems.com>
2021-12-21 15:45:27 -0500

    update to components

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,87 +1,23 @@
 <script>
-  let score1 = 0;
-  let score2 = 0;
-  let score3 = 0;
-  let score4 = 0;
-  let hand1  = 0;
-  let hand2  = 0;
-  let hand3  = 0;
-  let hand4  = 0;
-
-
-  function updateScore() {
-    score1 += hand1;
-    hand1 = 0; 
-  }
-
+ import Scorecard from './scorecard.svelte';
+ let refresh = "./refresh.png";
 </script>
 
 
-<img src="/heart.png" class="headerimg">
-<input type="text" id="player1Name" name="player1Name" value="Player 1:">
-<h1>{score1}</h1>
-<input type="number" bind:value={hand1}>
+<header>
+  <div>
+  <img src="/heart.png" class="headerimg">
+  <h1>Svelte Hearts</h1>
+</div>
+<div>
+  <img src="{refresh}" class="refresh">
+</div>
+</header>
 
-<input type="text" id="player2Name" name="player2Name" value="Player 2:">
-<h1>{score2}</h1>
-<input type="text" id="player3Name" name="player3Name" value="Player 3:">
-<h1>{score3}</h1>
-<input type="text" id="player4Name" name="player4Name" value="Player 4:">
-<h1>{score4}</h1>
-
-<button on:click={updateScore}>Submit!</button>
+  <Scorecard/>
 
 
-<style>
-  :global(body) {
-    font-family: Menlo, Consolas, Monaco, Liberation Mono, Lucida Console,
-      monospace;
-    background-color: #fafafa;
-    max-width: 650px;
-    margin: 32px auto;
-    padding: 0 16px;
-  }
-  h1 {
-    letter-spacing: -0.025em;
-  }
-  h2 {
-    font-size: 18px;
-  }
-  ul {
-    list-style: none;
-    padding: 0;
-    margin-top: 32px;
-  }
-  li {
-    border: 1px solid #eaeaea;
-    border-radius: 8px;
-    margin-bottom: 16px;
-    background-color: white;
-    transition: 0.15s box-shadow ease-in-out;
-  }
-  li:hover {
-    box-shadow: 0 5px 10px rgba(0, 0, 0, 0.12);
-  }
-  p {
-    color: #666;
-    font-size: 14px;
-    line-height: 1.75;
-  }
-  a {
-    color: #0070f3;
-    text-decoration: none;
-  }
-  .card-link {
-    padding: 8px 24px;
-    display: block;
-  }
-  .link {
-    transition: 0.15s text-decoration ease-in-out;
-    color: #0761d1;
-  }
-  .link:hover {
-    text-decoration: underline;
-  }
+<style>  
   .headerimg {
     height: 128px;
     top: 10px;
---
+++ src/routes/scorecard.svelte
@@ -0,0 +1,51 @@
+<script>
+  let score1 = 0;
+  let score2 = 0;
+  let score3 = 0;
+  let score4 = 0;
+  let hand1  = 0;
+  let hand2  = 0;
+  let hand3  = 0;
+  let hand4  = 0;
+
+
+  function updateScore() {
+    score1 += hand1;
+    score2 += hand2;
+    score3 += hand3;
+    score4 += hand4;
+    hand1 = 0;
+    hand2 = 0;
+    hand3 = 0;
+    hand4 = 0; 
+  }
+
+</script>
+
+<div>
+  <input type="text" id="player1Name" name="player1Name" value="Player 1:">
+  <h1>{score1}</h1>
+  <input type="number" bind:value={hand1}>
+</div>
+
+<div>
+  <input type="text" id="player2Name" name="player2Name" value="Player 2:">
+  <h1>{score2}</h1>
+  <input type="number" bind:value={hand2}>
+</div>
+
+<div>
+  <input type="text" id="player3Name" name="player3Name" value="Player 3:">
+  <h1>{score3}</h1>
+  <input type="number" bind:value={hand3}>
+</div>
+
+<div>
+  <input type="text" id="player4Name" name="player4Name" value="Player 4:">
+  <h1>{score4}</h1>
+  <input type="number" bind:value={hand4}>
+</div>
+
+<button on:click={updateScore}>Submit!</button>
+
+
